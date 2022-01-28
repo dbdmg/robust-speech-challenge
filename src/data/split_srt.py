817935e3845daf6695f21da3c1ad3941cd1a08a8
Flavio Giobergia <flavio.giobergia@gmail.com>
2022-01-28 15:41:25 +0100

    wrapped extract_chunks to store output to file(s)

--- src/data/split_srt.py
+++ src/data/split_srt.py
@@ -1,8 +1,11 @@
 import librosa
 import pysrt
 from datetime import datetime
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 import numpy as np
+import pandas as pd
+import os
+import soundfile
 
 def get_index(t: float, sr: int) -> int:
     return int(t * sr)
@@ -50,3 +53,46 @@ def extract_chunks(audio_file: str, srt_file: str) -> Tuple[List[Tuple[np.ndarra
         # (e.g. to remove newlines, brackets, etc.)
         chunks.append((audio_chunk, sub.text))
     return chunks, sr
+
+
+def store_chunks(audio_file: str, srt_file: str, csv_file: Optional[str]=None, outdir: str=".", out_extension: str="ogg") -> None:
+    """Split an audio file based on its subtitles. Then, store the chunks in separate files
+    and create a (csv) mapping file (audio file -> subtitle).
+
+    Parameters
+    ----------
+    audio_file : str
+        the path to the audio file (must be a format supported by librosa.load)
+    srt_file : str
+        the path to the subtitles file (must be a .srt file)
+    csv_file : str
+        the output csv file. If not specified, it is stored in `outdir` and has
+        the same name as `audio_file` (minus the extension, which will be .csv)
+    outdir : str
+        the directory where the output files should be stored. `.` by default
+    out_extension : str
+        the extension of the output audio files. `ogg` by default. It should be
+        an extension supported by `soundfile.write`.
+
+    Returns
+    -------
+    None
+
+    """
+    basename = os.path.splitext(os.path.basename(audio_file))[0]
+
+    if csv_file is None:
+        csv_file = os.path.join(outdir, f"{basename}.csv")
+
+    chunks, sr = extract_chunks(audio_file, srt_file)
+
+    rows = []
+    for i, (chunk, srt) in enumerate(chunks):
+        outfile = os.path.join(outdir, f"{basename}.{i}.{out_extension}")
+        soundfile.write(outfile, chunk, sr)
+        rows.append((outfile, srt))
+
+    df = pd.DataFrame(data=rows, columns=["audio", "subs"])
+    df.to_csv(csv_file, index=False)
+
+
