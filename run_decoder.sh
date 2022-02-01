61af1271f363f2eab854048301d54a72ef56890f
Luca Colomba <lc.colomba@gmail.com>
2022-02-01 18:04:49 +0100

    added bash script to run decode

---
+++ run_decoder.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python run_decoder.py \
+    --dataset_name="mozilla-foundation/common_voice_7_0" \
+    --use_auth_token \
+    --dataset_config_name="it" \
+    --train_split_name="test[50%:52%]" \
+    --model_name_or_path="dbdmg/wav2vec2-xls-r-300m-italian-augmented" \
+    --kenlm "../5gram-it-cv-eos.arpa"
+    
