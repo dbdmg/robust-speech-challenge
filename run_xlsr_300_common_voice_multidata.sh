047a59dbc2194d9fff67f1f17afe8b7313668c3c
MorenoLaQuatra <moreno.laquatra@gmail.com>
2022-02-02 17:26:56 +0000

    Added scripts for CV-LS

---
+++ run_xlsr_300_common_voice_multidata.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+python run_speech_recognition_ctc_bnb_multidata.py \
+	--dataset_name "mozilla-foundation/common_voice_7_0" "multilingual_librispeech" \
+	--model_name_or_path="facebook/wav2vec2-xls-r-300m" \
+	--dataset_config_name="it" \
+	--output_dir="../wav2vec2-xls-r-300m-italian-multidata" \
+	--overwrite_output_dir \
+	--num_train_epochs="8" \
+	--per_device_train_batch_size="32" \
+	--per_device_eval_batch_size="32" \
+	--learning_rate="3e-4" \
+	--warmup_steps="500" \
+	--evaluation_strategy="steps" \
+	--text_column_name="sentence" \
+	--length_column_name="input_length" \
+	--save_steps="400" \
+	--eval_steps="400" \
+	--layerdrop="0.0" \
+	--save_total_limit="3" \
+	--freeze_feature_encoder \
+	--gradient_checkpointing \
+	--chars_to_ignore , ? . ! - \; \: \" “ % ‘ ” � \
+	--fp16 \
+	--group_by_length \
+	--push_to_hub \
+    --use_auth_token \
+	--hub_model_id="dbdmg/wav2vec2-xls-r-300m-italian-augmented_multidata" \
+    --data_augmentation \
+    --noise_root_path="/workspace/helper_data/UrbanSound8K/audio/" \
+    --dataloader_num_workers="16" \
+    --ctc_zero_infinity \
+    --dataloader_pin_memory \
+	--do_train --do_eval \
+    --eval_split_name="validation" \
+    --train_split_name="train"
