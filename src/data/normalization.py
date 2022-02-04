8857f59dd641119d4c30eaed4e1993922c5d4a46
Flavio Giobergia <flavio.giobergia@gmail.com>
2022-02-04 11:01:24 +0100

    added number convertion to normalization

--- src/data/normalization.py
+++ src/data/normalization.py
@@ -1,4 +1,6 @@
 """Normalize textual strings"""
+from num2words import num2words
+import re
 
 #TODO I guess it exists a precompiled list of italian mappings
 SYMBOL_TO_STR = {
@@ -7,14 +9,22 @@ SYMBOL_TO_STR = {
     "£": "sterline",
 }
 
-def normalize_string(text: str, to_lower=True, expand_symbols=True):
+def converter(match: re.Match) -> str:
+    # adding leading and trailing spaces
+    return f" {num2words(int(match[1]), lang='it')} "
+
+def normalize_string(text: str, to_lower : bool = True, expand_symbols : bool = True, convert_numbers : bool = True):
     """Normalize a string"""
     
     if to_lower:
-        new = text.lower()
+        text = text.lower()
     
     if expand_symbols:
         for sym, t in SYMBOL_TO_STR.items():
-            new = new.replace(sym, t)
+            text = text.replace(sym, t)
 
-    return new
+    if convert_numbers:
+        match_num_re = re.compile(r"(\d+)")
+        text = match_num_re.sub(converter, text)
+
+    return text
